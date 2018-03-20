52bb5f1cac74b67b47c1f3460f03a68f9388c9c9
Monpie <kevin.pidemont@sfr.fr>
2018-03-20 14:10:03 +0100

    mise à jour log

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,9 @@ class WelcomeController < ApplicationController
 
       flash[:info] = "Bienvenue #{@current_user.nom} !"
       create_file
+      #flash[:info] = request.fullpath #Récupère seulement la route
+      #flash[:info] = request.env['PATH_INFO'] #Meme chose que fullpath
+      #request.original_url #Récupère l'URL original complète
       redirect_to "/anomalie"
     else
       flash[:info] = "Échec de la connexion"
@@ -16,9 +19,10 @@ class WelcomeController < ApplicationController
   end
 
   def create_file
-    file_name = "/home/kevin/Documents/log.txt"
+    file_name = "#{Dir.home}/Documents/log.txt"
+
     if not File.exist? file_name
-      @log_file = File.new(file_name, File::CREAT|File::TRUNC|File::RDWR, 0644)
+      @log_file = File.new(file_name, File::CREAT|File::TRUNC|File::RDWR, 0644) #Créer un fichier "log.txt"
       time = Time.now
       @log_file << "[#{Time.utc time.year, time.month, time.day, time.hour, time.min, time.sec}] Fichier créé"
       @log_file.close
@@ -28,6 +32,5 @@ class WelcomeController < ApplicationController
       @log_file << "\n[#{Time.utc time.year, time.month, time.day, time.hour, time.min, time.sec}] user : #{@current_user.nom}, ip : #{request.remote_ip}"
       @log_file.close
     end
-
   end
 end
